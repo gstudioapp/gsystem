5d305f69a5a9ba0cb9f6dff260e7f304b2ad80ae
Thiago Capelo <thiagokpelo@gmail.com>
2020-06-20 09:16:36 -0300

    refactor: create script to lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,11 @@ module.exports = {
     project: './tsconfig.json'
   },
   rules: {
-    '@typescript-eslint/no-unused-vars': 'off'
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/triple-slash-reference': 'off',
+    '@typescript-eslint/no-floating-promises': 'off'
   }
 }
