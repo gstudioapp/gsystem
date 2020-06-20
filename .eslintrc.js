3b1c97a852eb7e3366734b9ce1423999af40535b
Vinicius Sales <vinicius.2010.s@gmail.com>
2020-06-20 13:29:11 -0300

    refactor: eslint sort import rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,19 @@
 module.exports = {
   extends: ['prettier', 'standard-with-typescript'],
-  plugins: ['prettier'],
+  plugins: ['prettier', 'sort-imports-es6-autofix'],
   parserOptions: {
     project: './tsconfig.json'
   },
   rules: {
     'prettier/prettier': 'error',
+    'sort-imports-es6-autofix/sort-imports-es6': [
+      2,
+      {
+        ignoreCase: false,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single']
+      }
+    ],
     '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/consistent-type-definitions': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
