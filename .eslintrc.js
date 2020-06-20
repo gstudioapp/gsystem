3437fb1a50f58314f42a33a31656fff085368f86
Thiago Capelo <thiagokpelo@gmail.com>
2020-06-20 08:54:38 -0300

    chore: add husky and lint staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,8 @@ module.exports = {
   extends: 'standard-with-typescript',
   parserOptions: {
     project: './tsconfig.json'
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off'
   }
 }
