376ab1d134ea1c49b41411d536f93641b924bbe9
Thiago Kpelo <thiago.kpelo@loft.com.br>
2020-06-20 11:57:17 -0300

    feat: add button component module

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { action } from '@storybook/addon-actions'
+
+import Button from './Button'
+
+export default {
+  title: 'Button',
+  component: Button
+}
+
+export const Text = () => <Button onClick={action('clicked')}>Hello Button</Button>
+
+export const Emoji = () => (
+  <Button onClick={action('clicked')} variantColor="teal" variant="solid">
+    <span role="img" aria-label="so cool">
+      😎
+    </span>
+      Click me
+  </Button>
+)
