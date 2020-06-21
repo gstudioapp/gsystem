d2a9e732f2fac0f45aaed60054cdb66dc2cd2682
Thiago Capelo <thiagokpelo@gmail.com>
2020-06-21 10:51:59 -0300

    feat: add utils storybook module with StoryBookWrapper

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import { addDecorator } from '@storybook/react';
-import { CSSReset, ThemeProvider } from '@chakra-ui/core';
 
-import customTheme from '../src/theme/theme';
+import StoryBookWrapper from './utils/StoryBookWrapper';
 
-const StoryWithThemeProvider = ({ children }) => (
-    <ThemeProvider theme={customTheme}>
-        <CSSReset />
-        {children}
-    </ThemeProvider>
-)
-
-addDecorator(storyFn => <StoryWithThemeProvider>{storyFn()}</StoryWithThemeProvider>);
+addDecorator(storyFn => <StoryBookWrapper>{storyFn()}</StoryBookWrapper>);
---
+++ .storybook/utils/StoryBookWrapper.tsx
@@ -0,0 +1,14 @@
+import {CSSReset} from "@chakra-ui/core";
+import React, {FC} from "react";
+
+import customTheme from "../../src/theme/theme";
+import ThemeProvider from "../../src/components/ThemeProvider";
+
+const StoryBookWrapper: FC = ({ children }) => (
+  <ThemeProvider theme={customTheme}>
+    <CSSReset />
+    {children}
+  </ThemeProvider>
+)
+
+export default StoryBookWrapper
