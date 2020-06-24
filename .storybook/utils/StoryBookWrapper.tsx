2a579e6a88ebb02b52f0877586f3b70497d880b4
Thiago Capelo <thiagokpelo@gmail.com>
2020-06-24 20:35:38 -0300

    refactor: use ThemeProvider from components

--- .storybook/utils/StoryBookWrapper.tsx
+++ .storybook/utils/StoryBookWrapper.tsx
@@ -2,7 +2,7 @@ import {CSSReset} from "@chakra-ui/core";
 import React, {FC} from "react";
 
 import customTheme from "../../src/theme/theme";
-import ThemeProvider from "../../src/components/ThemeProvider";
+import { ThemeProvider } from "../../src/components";
 
 const StoryBookWrapper: FC = ({ children }) => (
   <ThemeProvider theme={customTheme}>
