0bfa5bed68784d981ae5b136e5a93c7d23d50a02
Thiago Capelo <thiagokpelo@gmail.com>
2020-08-22 22:39:01 -0300

    refactor: add eslint-disabled to any

--- packages/components/src/ThemeProvider/ThemeProvider.tsx
+++ packages/components/src/ThemeProvider/ThemeProvider.tsx
@@ -7,6 +7,7 @@ import { theme as gTheme, GSystemThemeType } from '../theme';
  * Primary UI component for user interaction
  */
 export const ThemeProvider: FC<{ theme?: GSystemThemeType }> = ({ children, theme = gTheme }) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return <ChakraThemeProvider theme={theme as any}>{children}</ChakraThemeProvider>;
 };
 
