da80c69b9c1178d0cd0cf8828f843eed9a1a38fd
Thiago Capelo <thiagokpelo@gmail.com>
2020-09-05 10:51:20 -0300

    refactor: fix eslint script

--- packages/components/src/Button/protocols.ts
+++ packages/components/src/Button/protocols.ts
@@ -6,8 +6,8 @@ import {
   size,
   variant,
   variantColor,
+  ComponentProtocol,
 } from '@gsystem/protocols';
-import { ComponentProtocol } from '@gsystem/protocols/dist/types';
 
 type ButtonThemePropsProtocol = VariantColorProtocol;
 type ButtonEditorPropsProtocol = SizeProtocol | VariantColorProtocol | IsLoadingProtocol;
