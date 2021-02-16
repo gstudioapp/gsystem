8455fc9f2844a091a57906ac61ad479382c6f378
Gabriel <gabrieldeoliveira.gdo23@gmail.com>
2021-02-16 01:20:20 -0300

    feat: done input component styling

--- packages/ds/src/theme/components/input.ts
+++ packages/ds/src/theme/components/input.ts
@@ -3,23 +3,19 @@ import { InputProps } from '@chakra-ui/react';
 
 import { ThemeComponent } from '../../types/theme-component';
 import { getColorSchemeOrDefault } from '../utils/variants';
+import { colors } from '../foundations';
 
 export type InputSizes = 'sm' | 'md' | 'lg' | string;
 export type InputVariants = 'default' | null;
 
 export const Input: ThemeComponent<InputSizes, InputVariants, InputProps> = {
-  baseStyle: ({ colorScheme }) => {
-    const colorSchemaDefault = getColorSchemeOrDefault({
-      colorScheme,
-      colorSchemeDefault: 'ui',
-    });
-
-    return {
-      borderRadius: 'xs',
-      border: 'sm',
-      borderColor: `${colorSchemaDefault}.200`,
-    };
-  },
+  baseStyle: () => ({
+    px: '3',
+    outline: 'none',
+    borderRadius: 'xs',
+    fontFamily: 'Helvetica',
+    transitionDuration: '0.2s',
+  }),
 
   sizes: {
     sm: {
@@ -37,8 +33,40 @@ export const Input: ThemeComponent<InputSizes, InputVariants, InputProps> = {
   },
 
   variants: {
-    default: () => {
-      return {};
+    default: ({ colorScheme }) => {
+      const colorSchemaDefault = getColorSchemeOrDefault({
+        colorScheme,
+        colorSchemeDefault: 'ui',
+      });
+
+      return {
+        border: 'sm',
+        color: `${colorSchemaDefault}.700`,
+        borderColor: `${colorSchemaDefault}.200`,
+        _placeholder: {
+          color: `${colorSchemaDefault}.500`,
+        },
+        _hover: {
+          borderColor: `${colorSchemaDefault}.500`,
+        },
+        _focus: {
+          border: 'md',
+          borderColor: 'primary.500',
+          boxShadow: `0px 0px 0px 3px ${colors.primary[100]}`,
+        },
+        _disabled: {
+          cursor: 'not-allowed',
+          _hover: {
+            borderColor: `${colorSchemaDefault}.200`,
+          },
+        },
+        _invalid: {
+          borderColor: 'supportA.600',
+          _focus: {
+            boxShadow: `0px 0px 0px 3px ${colors.supportA[100]}`,
+          },
+        },
+      };
     },
   },
 
