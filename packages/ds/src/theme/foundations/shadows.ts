bb4f95eb280a8ec4672852dd02727e89a0fc77a6
Gabriel <gabrieldeoliveira.gdo23@gmail.com>
2021-02-23 19:36:30 -0300

    refactor: updated the foundations tokens with the correct keys and values

--- packages/ds/src/theme/foundations/shadows.ts
+++ packages/ds/src/theme/foundations/shadows.ts
@@ -1,7 +1,6 @@
 export const shadows = {
-  sm: '0px 4px 8px rgba(0, 0, 0, 0.06)',
   base: '0px 1px 2px rgba(0, 0, 0, 0.06), 0px 1px 3px rgba(0, 0, 0, 0.1)',
-  md: '0px 4px 16px rgba(0, 0, 0, 0.06)',
-  lg: '0px 8px 32px rgba(0, 0, 0, 0.06)',
-  xl: '0px 16px 64px rgba(0, 0, 0, 0.06)',
+  medium: '0px 4px 4px rgba(0, 0, 0, 0.25), 0px 2px 4px rgba(0, 0, 0, 0.06)',
+  large: '0px 10px 15px rgba(0, 0, 0, 0.1), 0px 4px 6px rgba(0, 0, 0, 0.05)',
+  primary: '0px 1px 3px rgba(133, 47, 255, 0.3)',
 };
