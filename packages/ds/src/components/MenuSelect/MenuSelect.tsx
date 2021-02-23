3e1c93d4f7793c1fecbdf996e9091944a5c38325
Gabriel <gabrieldeoliveira.gdo23@gmail.com>
2021-02-23 02:22:35 -0300

    feat: provided component items render and change event plus component stories

--- packages/ds/src/components/MenuSelect/MenuSelect.tsx
+++ packages/ds/src/components/MenuSelect/MenuSelect.tsx
@@ -1,24 +1,79 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import {
+  Button as ChakraButton,
   Menu as ChakraMenu,
+  MenuButton as ChakraMenuButton,
+  MenuList as ChakraMenuList,
+  MenuItem as ChakraMenuItem,
   MenuProps as ChakraMenuProps,
   useMultiStyleConfig,
 } from '@chakra-ui/react';
 
 import { MenuSelectSizes, MenuSelectVariants } from '../../theme';
+import { TriangleDownIcon, TriangleUpIcon } from '@chakra-ui/icons';
 
-export interface MenuSelectProps {
-  size?: MenuSelectSizes;
-  variant?: MenuSelectVariants | ChakraMenuProps['variant'];
+interface MenuSelectOption {
+  label: string;
+  value: unknown;
 }
 
-export const MenuSelect: FC<MenuSelectProps> = ({ size, variant, ...props }) => {
-  const styles = useMultiStyleConfig('MenuSelect', { size, variant });
+export interface MenuSelectProps extends ChakraMenuProps {
+  placeholder?: string;
+  size?: MenuSelectSizes;
+  variant?: MenuSelectVariants;
+  options?: Array<MenuSelectOption>;
+  onChange?: (option: MenuSelectOption) => void;
+}
+
+export const MenuSelect: FC<MenuSelectProps> = ({
+  size,
+  variant,
+  colorScheme,
+  placeholder,
+  options,
+  onChange,
+  ...props
+}) => {
+  const [selectedOption, setSelectedOption] = useState<MenuSelectOption>();
+  const styles = useMultiStyleConfig('MenuSelect', { size, variant, colorScheme });
+
+  function handleOnChange(option: MenuSelectOption) {
+    setSelectedOption(option);
+    onChange(option);
+  }
 
   return (
-    <ChakraMenu data-testid="menu-select" {...props}>
-      aaa
-    </ChakraMenu>
+    <>
+      <ChakraMenu matchWidth data-testid="menu-select" {...props}>
+        {({ isOpen }) => (
+          <>
+            <ChakraMenuButton
+              sx={styles['menu-button']}
+              as={ChakraButton}
+              fontWeight={selectedOption ? 'bold' : 'normal'}
+              rightIcon={
+                isOpen ? <TriangleUpIcon boxSize="0.5rem" /> : <TriangleDownIcon boxSize="0.5rem" />
+              }
+            >
+              {selectedOption ? selectedOption.label : placeholder}
+            </ChakraMenuButton>
+            {options?.length > 0 && (
+              <ChakraMenuList sx={styles['menu-list']}>
+                {options.map((option, idx) => (
+                  <ChakraMenuItem
+                    key={idx}
+                    sx={styles['menu-item']}
+                    onClick={() => handleOnChange(option)}
+                  >
+                    {option.label}
+                  </ChakraMenuItem>
+                ))}
+              </ChakraMenuList>
+            )}
+          </>
+        )}
+      </ChakraMenu>
+    </>
   );
 };
 
--- packages/ds/src/components/MenuSelect/__stories__/MenuSelect.stories.tsx
+++ packages/ds/src/components/MenuSelect/__stories__/MenuSelect.stories.tsx
@@ -6,7 +6,31 @@ import { MenuSelect, MenuSelectProps } from '../MenuSelect';
 export default {
   title: 'Atoms/MenuSelect',
   component: MenuSelect,
-  argTypes: {},
+  args: {
+    options: [
+      {
+        label: 'Option 1',
+        value: 'foo',
+      },
+      {
+        label: 'Option 2',
+        value: 'foo',
+      },
+      {
+        label: 'Option 3',
+        value: 'foo',
+      },
+    ],
+    onChange: (option) => console.log(option),
+  },
+  argTypes: {
+    placeholder: {
+      defaultValue: 'Choose an option',
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta;
 
 export const Default: Story<MenuSelectProps> = (args) => <MenuSelect {...args} />;
