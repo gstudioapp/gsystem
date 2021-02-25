bbd59497ad0a13d0da59b67091d73812d0f3e9a6
Gabriel <gabrieldeoliveira.gdo23@gmail.com>
2021-02-24 22:33:16 -0300

    feat: added icon component and stories

--- packages/ds/src/components/Icon/Icon.tsx
+++ packages/ds/src/components/Icon/Icon.tsx
@@ -1,16 +1,26 @@
 import React, { FC } from 'react';
-import { IconProps as ChakraIconProps } from '@chakra-ui/react';
+import { IconProps as ChakraIconProps, useStyleConfig } from '@chakra-ui/react';
+
+import { merge } from 'lodash';
 
 import { IconName } from '../../types';
-import { IconSizes } from '../../theme/components/icon';
+import { IconSizes, IconVariants } from '../../theme';
+
+import { coreIcons } from './CoreIcons';
 
 export interface IconProps extends ChakraIconProps {
   name?: IconName;
   size?: IconSizes;
+  variant?: IconVariants;
 }
 
-export const Icon: FC<IconProps> = () => {
-  return <>hello world</>;
+export const Icon: FC<IconProps> = ({ name, size, variant, ...props }) => {
+  const styles = useStyleConfig('Icon', { size, variant });
+  const icons = merge(coreIcons);
+
+  const RenderedIcon = icons[name] ?? icons['question-outline'];
+
+  return <RenderedIcon sx={styles} {...props} />;
 };
 
 Icon.displayName = 'Icon';
