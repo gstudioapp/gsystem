1618b8cd67b30645d17113f7aee8d6fd876cd266
Gabriel <gabrieldeoliveira.gdo23@gmail.com>
2021-02-24 22:34:08 -0300

    feat: added box-size variations to the icon

--- packages/ds/src/theme/components/icon.ts
+++ packages/ds/src/theme/components/icon.ts
@@ -2,11 +2,23 @@ import { IconProps as ChakraIconProps } from '@chakra-ui/react';
 
 import { ThemeComponent } from '../../types';
 
-export type IconSizes = 'sm' | 'md' | 'lg' | string;
-export type IconVariants = 'default' | null;
+export type IconSizes = 'sm' | 'md' | 'lg';
+export type IconVariants = 'default';
 
 export const Icon: ThemeComponent<IconSizes, IconVariants, ChakraIconProps> = {
   baseStyle: () => ({}),
+
+  sizes: {
+    sm: {
+      boxSize: '0.75rem',
+    },
+    md: {
+      boxSize: '1.125rem',
+    },
+    lg: {
+      boxSize: '1.5rem',
+    },
+  },
 
   defaultProps: {
     size: 'md',
