19bfb576626ef53fcf7bf49ffb1becbcf1d1c2af
Gabriel <gabrieldeoliveira.gdo23@gmail.com>
2021-02-26 17:03:36 -0300

    feat: done card component and stories

--- packages/ds/src/components/Card/Card.tsx
+++ packages/ds/src/components/Card/Card.tsx
@@ -1,13 +1,19 @@
 import React, { FC } from 'react';
 import { Box as ChakraBox, BoxProps as ChakraBoxProps, useStyleConfig } from '@chakra-ui/react';
 
+import { CardSizes, CardVariants } from '../../theme';
+
 export interface CardProps extends ChakraBoxProps {
-  variant: string;
-  size: string;
+  variant?: CardVariants;
+  size?: CardSizes;
 }
 
-export const Card: FC<CardProps> = ({ size, variant, ...props }) => {
+export const Card: FC<CardProps> = ({ size, variant, children, ...props }) => {
   const styles = useStyleConfig('Card', { size, variant });
 
-  return <ChakraBox sx={styles} {...props} />;
+  return (
+    <ChakraBox sx={styles} {...props}>
+      {children}
+    </ChakraBox>
+  );
 };
--- packages/ds/src/components/Card/__stories__/Card.stories.tsx
+++ packages/ds/src/components/Card/__stories__/Card.stories.tsx
@@ -6,6 +6,21 @@ import { Card, CardProps } from '../Card';
 export default {
   title: 'Display/Card',
   component: Card,
+  argTypes: {
+    boxShadow: {
+      defaultValue: 'base',
+      control: {
+        type: 'select',
+        options: ['base', 'medium', 'large', 'primary', 'none'],
+      },
+    },
+    size: {
+      defaultValue: 'md',
+      control: {
+        type: 'select',
+      },
+    },
+  },
 } as Meta;
 
-export const Default: Story<CardProps> = (args) => <Card {...args} />;
+export const Default: Story<CardProps> = (args) => <Card {...args}>Example Content</Card>;
